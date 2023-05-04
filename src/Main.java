11b72745d4cb04ecd6d0fa36ba2353188bd60a23
elnur <elnurkerimli2@gmail.com>
2023-05-04 16:07:25 +0300

    file uptade

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path="C:\\elnur\\brfile";
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter name");
+            String st1 = sc.nextLine();
+
+            System.out.println("Enter surname");
+            String st2 = sc.nextLine();
+
+            System.out.println("Enter age");
+            String st3 = sc.nextLine();
+
+            System.out.println("Enter address");
+            String st4 = sc.nextLine();
+
+            File f = new File(path);
+            if (!f.exists()) {
+                f.mkdir();
+            }
+
+            FileWriter fw = new FileWriter(f + File.separator + "info.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(st1 + " " + st2 + " " + st3 + " " + st4);
+            bw.newLine();
+            bw.close();
+
+            FileReader fr = new FileReader(path + File.separator + "info.txt");
+            BufferedReader br = new BufferedReader(fr);
+            while (br.ready()) {
+                String data = br.readLine();
+                System.out.println(data);
+            }
+        }
+
+    }
+}
